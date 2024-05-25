eaca98b286592e29b645ab99e9160aa26c97aa13
Richard Moore <dickymoore@gmail.com>
2024-05-25 17:45:01 +0100

    skeleton added

--- bearcraftmusic/src/components/Footer.js
+++ bearcraftmusic/src/components/Footer.js
@@ -1,31 +1,74 @@
-import React from 'react';
-import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa'; 
-import './Footer.css';
-
-const Footer = () => {
-  return (
-    <footer className="footer">
-      <div className="footer-section">
-        <h3>Links</h3>
-        <ul>
-          <li><a href="/About">About</a></li>
-          <li><a href="/Shop">Shop</a></li>
-          <li><a href="/Video">Video</a></li>
-        </ul>
-      </div>
-      <div className="footer-section">
-        <h3>Connect</h3>
-        <ul className="social-icons">
-          <li><a href="https://facebook.com" target="_blank" rel="noopener noreferrer"><FaFacebook /></a></li>
-          <li><a href="https://twitter.com" target="_blank" rel="noopener noreferrer"><FaTwitter /></a></li>
-          <li><a href="https://instagram.com" target="_blank" rel="noopener noreferrer"><FaInstagram /></a></li>
-        </ul>
-      </div>
-      <div className="footer-section">
-        <p>&copy; {new Date().getFullYear()} Bearcraft Music. All rights reserved.</p>
-      </div>
-    </footer>
-  );
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+  background-color: black;
 }
 
-export default Footer;
+.logo {
+  color: white;
+  text-decoration: none;
+  font-size: 1.5em;
+  display: flex;
+  align-items: center;
+}
+
+.logo-image {
+  height: auto;
+  max-height: 50px;
+}
+
+.menu {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  margin: 0;
+  padding: 0;
+}
+
+.menu a {
+  color: white;
+  text-decoration: none;
+  font-size: 20px;
+  margin-left: 10px; /* Reduced margin-left from 20px to 10px */
+}
+
+.black-bg {
+  background-color: black;
+}
+
+.white-bg {
+  background-color: white;
+}
+
+.header a {
+  color: #fff;
+  text-decoration: none;
+  padding: 3px 5px;
+}
+
+.header a:hover {
+  background-color: #555;
+  border-radius: 5px;
+}
+
+.header .social-icons {
+  display: flex;
+  align-items: center;
+  gap: 3px;
+  padding: 5px;
+  height: 100%;
+}
+
+.header .social-icons a {
+  color: white; /* Icon color white */
+  font-size: 1.2em;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.header .social-icons a:hover {
+  color: #7d7d7d; /* Grey hover color */
+}
--- bearcraftmusic/src/pages/Home.js
+++ bearcraftmusic/src/pages/Home.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import './Home.css';
 
 function Home() {
   console.log("Home component rendered");
   return (
-    <div>
-      <h1>Home</h1>
-      <p>Welcome to the home page!</p>
+    <div className="home">
+      <div className="content">
+        <div className="video-container">
+          <iframe 
+            src="https://www.youtube.com/embed/AlnioEJE8tg" 
+            title="YouTube video player" 
+            frameBorder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            allowFullScreen
+          ></iframe>
+        </div>
+        <img src="/images/Dicky-35-Contrast.jpg" alt="Bearcraft" className="home-image" />
+      </div>
     </div>
   );
 }
