b3b5fe83a8c14bbde125e18deaf7210ef5746b9f
Richard Moore <dickymoore@gmail.com>
2024-05-25 19:02:14 +0100

    skeleton added

--- bearcraftmusic/src/pages/Video.js
+++ bearcraftmusic/src/pages/Video.js
@@ -1,11 +1,31 @@
-// src/pages/Video.js
 import React from 'react';
+import './Video.css';
 
 function Video() {
+  const videoLinks = [
+    "https://www.youtube.com/embed/eUpxFOJpD4k",
+    "https://www.youtube.com/embed/pMqEHrnux3k",
+    "https://www.youtube.com/embed/trl3Hn4LfTo",
+    "https://www.youtube.com/embed/8vawrS8QMuA",
+    "https://www.youtube.com/embed/mFnz0Y2xVwE",
+    "https://www.youtube.com/embed/aUmo9gXerOc"
+  ];
+
   return (
-    <div>
-      <h1>Video</h1>
-      <p>This is the Video page.</p>
+    <div className="video-page">
+      <div className="video-grid">
+        {videoLinks.map((link, index) => (
+          <div key={index} className="video-container">
+            <iframe 
+              src={link} 
+              title={`YouTube video player ${index}`} 
+              frameBorder="0" 
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+              allowFullScreen
+            ></iframe>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
