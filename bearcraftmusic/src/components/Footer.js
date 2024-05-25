f084b87a5904d5fb85e36aa37c11f22f75887990
Richard Moore <dickymoore@gmail.com>
2024-05-25 17:49:56 +0100

    skeleton added

--- bearcraftmusic/src/components/Footer.js
+++ bearcraftmusic/src/components/Footer.js
@@ -1,74 +1,58 @@
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 10px 20px;
-  background-color: black;
+import React from 'react';
+import { FaFacebookF, FaTwitter, FaInstagram, FaYoutube, FaSoundcloud, FaSpotify } from 'react-icons/fa';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="footer-section links-section">
+          <h3>Links</h3>
+          <ul>
+            <li><a href="/About">About</a></li>
+            <li><a href="/Shop">Shop</a></li>
+            <li><a href="/Video">Video</a></li>
+          </ul>
+        </div>
+        <div className="footer-section social-section">
+          <h3>Connect</h3>
+          <ul className="social-icons">
+            <li><a href="https://soundcloud.com/bearcraft" target="_blank" rel="noopener noreferrer"><FaSoundcloud /></a></li>
+            <li><a href="https://www.facebook.com/Bearcraft/" target="_blank" rel="noopener noreferrer"><FaFacebookF /></a></li>
+            <li><a href="https://twitter.com/bearcraftmusic" target="_blank" rel="noopener noreferrer"><FaTwitter /></a></li>
+            <li><a href="https://www.instagram.com/bearcraftmusic/" target="_blank" rel="noopener noreferrer"><FaInstagram /></a></li>
+            <li><a href="https://www.youtube.com/user/Bearcraftmusic" target="_blank" rel="noopener noreferrer"><FaYoutube /></a></li>
+            <li><a href="https://open.spotify.com/artist/63VxrRwKAytSfmlhI7SwN1" target="_blank" rel="noopener noreferrer"><FaSpotify /></a></li>
+          </ul>
+        </div>
+        <div className="footer-section subscribe-section">
+          <div id="mc_embed_signup">
+            <form action="https://bearcraftmusic.us10.list-manage.com/subscribe/post?u=e447d40fb160a6b602466cb1c&amp;id=0083f7fbc9&amp;f_id=00265ae5f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
+              <div id="mc_embed_signup_scroll">
+                <h2>Subscribe</h2>
+                <div className="mc-field-group">
+                  <input type="email" name="EMAIL" className="required email" id="mce-EMAIL" placeholder="Email address" required />
+                </div>
+                <div id="mce-responses" className="clear foot">
+                  <div className="response" id="mce-error-response" style={{display: "none"}}></div>
+                  <div className="response" id="mce-success-response" style={{display: "none"}}></div>
+                </div>
+                <div style={{position: "absolute", left: "-5000px"}} aria-hidden="true">
+                  <input type="text" name="b_e447d40fb160a6b602466cb1c_0083f7fbc9" tabIndex="-1" value="" />
+                </div>
+                <div className="clear foot">
+                  <input type="submit" name="subscribe" id="mc-embedded-subscribe" className="button" value="Subscribe" />
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <p>&copy; {new Date().getFullYear()} Bearcraft Music. All rights reserved.</p>
+      </div>
+    </footer>
+  );
 }
 
-.logo {
-  color: white;
-  text-decoration: none;
-  font-size: 1.5em;
-  display: flex;
-  align-items: center;
-}
-
-.logo-image {
-  height: auto;
-  max-height: 50px;
-}
-
-.menu {
-  list-style: none;
-  display: flex;
-  align-items: center;
-  margin: 0;
-  padding: 0;
-}
-
-.menu a {
-  color: white;
-  text-decoration: none;
-  font-size: 20px;
-  margin-left: 10px; /* Reduced margin-left from 20px to 10px */
-}
-
-.black-bg {
-  background-color: black;
-}
-
-.white-bg {
-  background-color: white;
-}
-
-.header a {
-  color: #fff;
-  text-decoration: none;
-  padding: 3px 5px;
-}
-
-.header a:hover {
-  background-color: #555;
-  border-radius: 5px;
-}
-
-.header .social-icons {
-  display: flex;
-  align-items: center;
-  gap: 3px;
-  padding: 5px;
-  height: 100%;
-}
-
-.header .social-icons a {
-  color: white; /* Icon color white */
-  font-size: 1.2em;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.header .social-icons a:hover {
-  color: #7d7d7d; /* Grey hover color */
-}
+export default Footer;
