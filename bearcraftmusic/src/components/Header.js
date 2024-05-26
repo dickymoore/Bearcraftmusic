9b2dc421d3657f90953ff57e2afe18d8cd5eba0a
dickymoorebjss <128724856+dickymoorebjss@users.noreply.github.com>
2024-05-26 12:27:32 +0100

    Hamburger menu added

--- bearcraftmusic/src/components/Header.js
+++ bearcraftmusic/src/components/Header.js
@@ -1,15 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { FaSoundcloud, FaFacebookF, FaTwitter, FaInstagram, FaYoutube, FaSpotify } from 'react-icons/fa';
+import { FaSoundcloud, FaFacebookF, FaTwitter, FaInstagram, FaYoutube, FaSpotify, FaBars } from 'react-icons/fa';
+import { slide as Menu } from 'react-burger-menu';
 import './Header.css';
+import './BurgerMenu.css';
 
 function Header() {
   const location = useLocation();
   const [isHomePage, setIsHomePage] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     setIsHomePage(location.pathname === '/');
   }, [location]);
+
+  const handleStateChange = (state) => {
+    setIsOpen(state.isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
 
   return (
     <header className={`header ${isHomePage ? 'dark-bg' : 'light-bg'}`}>
@@ -26,11 +37,31 @@ function Header() {
             <a href="https://soundcloud.com/bearcraft" target="_blank" rel="noopener noreferrer"><FaSoundcloud /></a>
             <a href="https://www.facebook.com/Bearcraft/" target="_blank" rel="noopener noreferrer"><FaFacebookF /></a>
             <a href="https://twitter.com/bearcraftmusic" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
-            <a href="https://www.instagram.com/bearcraftmusic/" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
+            <a href="https://www.instagram.com/bearcraftmusic" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
             <a href="https://www.youtube.com/user/Bearcraftmusic" target="_blank" rel="noopener noreferrer"><FaYoutube /></a>
             <a href="https://open.spotify.com/artist/63VxrRwKAytSfmlhI7SwN1" target="_blank" rel="noopener noreferrer"><FaSpotify /></a>
           </div>
         </ul>
+        <div className="hamburger-menu">
+          <FaBars onClick={() => setIsOpen(!isOpen)} />
+        </div>
+        <Menu right isOpen={isOpen} onStateChange={handleStateChange}>
+          <Link to="/" onClick={closeMenu} className="bm-logo">
+            <img src="/images/BearcraftWebLogo20ptFDF3EF.png" alt="Bearcraft" className="bm-logo-image" />
+          </Link>
+          <Link to="/Video" onClick={closeMenu}>Video</Link>
+          <Link to="/Audio" onClick={closeMenu}>Audio</Link>
+          <Link to="/About" onClick={closeMenu}>About</Link>
+          <Link to="/Shop" onClick={closeMenu}>Shop</Link>
+          <div className="social-icons">
+            <a href="https://soundcloud.com/bearcraft" target="_blank" rel="noopener noreferrer"><FaSoundcloud /></a>
+            <a href="https://www.facebook.com/Bearcraft/" target="_blank" rel="noopener noreferrer"><FaFacebookF /></a>
+            <a href="https://twitter.com/bearcraftmusic" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
+            <a href="https://www.instagram.com/bearcraftmusic" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
+            <a href="https://www.youtube.com/user/Bearcraftmusic" target="_blank" rel="noopener noreferrer"><FaYoutube /></a>
+            <a href="https://open.spotify.com/artist/63VxrRwKAytSfmlhI7SwN1" target="_blank" rel="noopener noreferrer"><FaSpotify /></a>
+          </div>
+        </Menu>
       </nav>
     </header>
   );
