1afeecfbd1c62aa8c3265cea6d0787612c13d8fe
dickymoorebjss <128724856+dickymoorebjss@users.noreply.github.com>
2024-05-31 10:24:33 +0100

    Removed unneccesary useLocation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // App.js
 
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Font from './components/Font';
